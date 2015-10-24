334a1172b3b14cc4f465f43a029eb2cb72128277
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-10-25 00:38:03 +0300

    [RO] Adaugă exercițiul 3 din CSIA: Tema 2

---
+++ Master/CSIA/tema2/exercitiul3.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+COLORS <- c("green", "red", "blue", "yellow", "orange", "pink", "brown")
+
+CLT <- function(n) {
+	# Scrieți o funcție care genereaza un număr de n eșantioane,
+	# fiecare eșantion de dimensiune 10000, folosind distribuția
+	# uniforma cu valori din intervalul [−10, 10].
+	# Numărul de eșantioane n este parametrul funcției.
+	# Funcția returneaza vectorul mediilor celor n eșantioane.
+	medii <- c()
+	for (index in 1:n) {
+		esantion <- runif(n=10000, min=-10, max=10)
+		medii[index] <- mean(esantion)
+	}
+	return(medii)
+} 
+
+histograma <- function(image_name, n) {
+	# Genereaza histograma pentru datele primite
+	png(image_name, width=4, height=4, units="in", res=300)
+	hist(CLT(n), col=COLORS)
+	dev.off()
+}
+
+main <- function() {
+	# Folosiți aceasta funcție pentru a construi histograma mediilor
+	# eșantioanelor când numărul acestora n este egal cu 1, 5, 10 și
+	# 100, respectiv.
+	# Observați modificarile histogramei când valorile lui n cresc.
+	for (valoare in c(1, 5, 10, 100)) {
+		histograma(paste('histograma-', valoare, '.png', seq=""), valoare)	
+	}
+}
+
+main()
