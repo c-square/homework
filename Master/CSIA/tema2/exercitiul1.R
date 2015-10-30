92ef0286a37ad576c0fdadaf39129b9e9cf3ebc8
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-10-30 08:04:26 +0200

    [RO] Modifică spațierea și sfârșitul de linie

--- Master/CSIA/tema2/exercitiul1.R
+++ Master/CSIA/tema2/exercitiul1.R
@@ -5,12 +5,13 @@ expression <- function(x) {
 }
 
 draw_plot <- function(start, stop) {
-	# Funcția afisează graficul funcției `f(x) = 2x` pe un interval 
-	# specificat [a, b].
-	# Se va utiliza culoarea roșie pentru grafic cu grosimea liniei 2.
-	# Graficul va trebui sa aibă ca titlul 'f(x)=2ˆx, x in [a,b]'.
+    # Funcția afisează graficul funcției `f(x) = 2x` pe un interval 
+    # specificat [a, b].
+    # Se va utiliza culoarea roșie pentru grafic cu grosimea liniei 2.
+    # Graficul va trebui sa aibă ca titlul 'f(x)=2ˆx, x in [a,b]'.
+    png("grafix-fx.png", width=4, height=4, units="in", res=300)
     title <- paste("f(x)=2^x, x in [", start, ", ", stop, "]", seq="")
-	curve(expression, start, stop, col="red", lwd=2, main=title)
+    curve(expression, start, stop, col="red", lwd=2, main=title)
 }
 
 draw_plot(1, 7)
--- Master/CSIA/tema2/exercitiul2.R
+++ Master/CSIA/tema2/exercitiul2.R
@@ -1,29 +1,29 @@
 #!/usr/bin/env Rscript
 
 init <- function(start, stop) {
-	# Pregătește setul de date
+    # Pregătește setul de date
     array <- c()
     for(index in start:stop){
-    	array[length(array) + 1] = 2 ^ index
+        array[length(array) + 1] = 2 ^ index
     }
-	return(array)
+    return(array)
 }
 
-generate_plot <- function(tip, p) {
-	# Generează un fișier .PNG cu graficul functiei bdinom. 
-	# Indicație: ?plot, ?title.
-	x <- init(0, 20)
-	png(tip, width=4, height=4, units="in", res=300)
-	plot(x, dbinom(x, 20, p))
-	dev.off()
+generate_plot <- function(filename, n, p) {
+    # Generează un fișier .PNG cu reprezentarea grafică a distribuției binomiale. 
+    # Indicație: ?plot, ?title.
+    x <- init(0, n)
+    png(filename, width=4, height=4, units="in", res=300)
+    plot(x, dbinom(x, n, p))
+    dev.off()
 }
 
 grafice <- function(){
-	# Generează graficele corespunzatoare distribuției binomiale `B(n, x, p)`,
-	# pentru n = 20 și `p = 0.1, 0.2, . . . , 0.9`.
-	for(index in seq(0.1, 0.9, 0.1)) {
-		generate_plot(paste('dbinom-', index, '.png', seq=""), index)
-	}
+    # Generează graficele corespunzatoare distribuției binomiale `B(n, x, p)`,
+    # pentru n = 20 și `p = 0.1, 0.2, . . . , 0.9`.
+    for(index in seq(0.1, 0.9, 0.1)) {
+        generate_plot(paste('dbinom-', index, '.png', seq=""), 20, index)
+    }
 }
 
 grafice()
